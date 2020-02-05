bfd910b43a19ebdd6197b0b4a5fd9379a5c3c1b4
sofianeaissani <55327622+sofianeaissani@users.noreply.github.com>
2020-02-05 13:21:14 +0100

    morphology

--- morpho.py
+++ morpho.py
@@ -20,29 +20,38 @@ def morpho(myFile, gain):
 
     global args
 
-    ## Load the image
+    ##### Load the image
     image = np.loadtxt(myFile) 
     image = np.float64(image)
     
-    ## ??
+    ##### ??
     log_stretch = LogStretch(a=10000.0)
 
-    ## Normalize the image
+    ##### Normalize the image
     image = normalize(image)
 
-    ## Compute and plot the segmentation map
+    ##### Compute and plot the segmentation map
+    if args.plot:
+        plt.figure()
+        left = plt.subplot(121)
+        right = plt.subplot(122)
+
     threshold = photutils.detect_threshold(image, 1.5)
-    npixels = 5  # minimum number of connected pixels (??)
+    npixels = 5  # minimum number of connected pixels
     segm = photutils.detect_sources(image, threshold, npixels)
-    # Keep only the largest segment (??)
+    # keep only the largest segment
     label = np.argmax(segm.areas) + 1
     segmap = segm.data == label
-    plt.imshow(segmap, origin='lower', cmap='gray')
+    if args.plot:
+        left.imshow(segmap, origin='lower', cmap='gray')
+
     segmap_float = ndi.uniform_filter(np.float64(segmap), size=10)
     segmap = segmap_float > 0.5
-    plt.imshow(segmap, origin='lower', cmap='gray')
+    if args.plot:
+        right.imshow(segmap, origin='lower', cmap='gray')
+        plt.show()
 
-    ## Compute the morphology using the gain and the segmap
+    ##### Compute the morphology using the gain and the segmap
     start = time.time()
     source_morphs = statmorph.source_morphology(image, segmap, gain=gain)
     print('Time: %g s.' % (time.time() - start))
@@ -134,9 +143,10 @@ def init_args():
 
     global parser, args
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="the image to analyse")
+    parser.add_argument("file", help="the file containing the image to analyse")
     parser.add_argument("-g", "--gain", type=int, default=1000.0, help="gain value, necessary to compute the morph statistics.")
     parser.add_argument("-s", "--save", action="store_true", help="save results in a txt file instead of printing it")
+    parser.add_argument("-p", "--plot", action="store_true", help="plot the segmentation map")
     args = parser.parse_args()
 
 if __name__ == "__main__":
