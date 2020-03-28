1cf81d6b5e7f8635950a4c4626e17ed308678c21
anthys <julbarres@gmail.com>
2020-03-28 15:13:27 +0100

    spherecorrel

--- create_matrice.py
+++ create_matrice.py
@@ -4,6 +4,12 @@ from libs.pic_process import *
 from libs.minkos import *
 from libs.matrices import *
 
+import matplotlib.pyplot as plt
+import matplotlib
+
+from mpl_toolkits.mplot3d import Axes3D
+from itertools import product, combinations
+
 def init_args():
 
     global parser, args
@@ -12,7 +18,7 @@ def init_args():
     parser.add_argument("dat_path", help='', type=str)
     parser.add_argument("conv_path", help='', type=str)
     parser.add_argument("-p", "--process", help='',action="store_true")
-    parser.add_argument("-n", "--no_treat", help='Pas de traitement et renomage des fichiers (si vous l"avez deja fait')',action="store_true")
+    parser.add_argument("-n", "--no_treat", help='Pas de traitement et renomage des fichiers (si vous l avez deja fait)',action="store_true")
     parser.add_argument("-l", "--load", help='Ne calcule pas la matrice et load le fichier deja fait',action="store_true")
 
     args = parser.parse_args()
@@ -33,9 +39,74 @@ def main():
     np.save("out", a)
 
   if args.process:
+    a = a.T
     b = process_matrix(a)
-
+    #cercle_correlation(b[1], b[0])
+    sphere_correlation(b[1],b[0])
   print(b)
+
+
+def cercle_correlation(matriceEspaces, valeursPropres, n=2):
+  assert n == 2
+  matriceEspaces = matriceEspaces.T
+  valeursPropres = [(valeursPropres[i],i) for i in range(len(valeursPropres))]
+  valeursPropres.sort()
+  valeursPropres = valeursPropres[:n]
+  print(valeursPropres[0], valeursPropres[1])
+
+
+  size_window = [5,5]
+  fig = plt.figure(figsize = (*size_window,))
+  #fig.add_subplot(111)
+  for espace in matriceEspaces:
+    x1 = espace[valeursPropres[0][1]]
+    y1 = espace[valeursPropres[1][1]]
+    plt.scatter(x1,y1)
+  circ1 = plt.Circle((0,0), 1, fill= False, color='r')
+  ax = plt.gcf().gca()
+  val = 1.1
+  ax.set_xlim([-val,val])
+  ax.set_ylim([-val,val])
+  plt.gcf().gca().add_artist(circ1)
+  plt.show()
+
+
+def sphere_correlation(matriceEspaces, valeursPropres, n=3):
+
+
+  fig = plt.figure()
+  ax = fig.gca(projection='3d')
+  ax.set_aspect("equal")
+
+  u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+  x = np.cos(u)*np.sin(v)
+  y = np.sin(u)*np.sin(v)
+  z = np.cos(v)
+  ax.plot_wireframe(x, y, z, color="r")
+
+  ax.scatter([0], [0], [0], color="g", s=100)
+
+  assert n == 3
+  matriceEspaces = matriceEspaces.T
+  matriceEspaces = matriceEspaces.real
+  valeursPropres = [(valeursPropres[i],i) for i in range(len(valeursPropres))]
+  valeursPropres.sort()
+  valeursPropres = valeursPropres[:n]
+  print(valeursPropres[0], valeursPropres[1], valeursPropres[2])
+
+  #fig.add_subplot(111)
+  for espace in matriceEspaces:
+    x1 = espace[valeursPropres[0][1]]
+    y1 = espace[valeursPropres[1][1]]
+    z1 = espace[valeursPropres[2][1]]
+    ax.scatter([x1],[y1],[z1], s=100)
+  #circ1 = plt.Circle((0,0), 1, fill= False, color='r')
+  #ax = plt.gcf().gca()
+  #val = 1.1
+  #ax.set_xlim([-val,val])
+  #ax.set_ylim([-val,val])
+  #plt.gcf().gca().add_artist(circ1)
+  plt.show()
 
 
 if __name__ == "__main__":
