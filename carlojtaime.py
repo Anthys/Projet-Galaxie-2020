b45bd0b6ce4691f7569e13a65af58bdc84e7aa65
Sofiane Aïssani <55327622+sofianeaissani@users.noreply.github.com>
2020-06-02 00:57:27 +0200

    delete one file
    delete one file

--- carlojtaime.py
+++
@@ -1,5 +0,0 @@
-import os, sys
-
-for i in os.listdir("sims"):
-  if i[-4:] == ".dat":
-   os.system("python3.5 carlo_putain_tu_deconne.py "+"sims/"+i + " -m 255 -cL 0.000001 -n -smooth 0 -s sofianejetaime")
